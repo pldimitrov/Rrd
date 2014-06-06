776efdf130fc81e814fe01a4e1af33d6958751a6
Plamen Dimitrov <dim.plamen@gmail.com>
2014-06-06 04:40:35 +0200

    basic package using librrd compiles!

--- src/Rrd.c
+++ src/Rrd.c
@@ -1,7 +1,23 @@
+#define RRD_EXPORT_DEPRECATED
+
+#include <rrd.h>
+//#include <rrd_format.h>
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
 #include <stdio.h>
+/*#include "rrd_tool.h"*/
+/*#include "rrd_rpncalc.h"*/
+/*#include "rrd_client.h"*/
+
+
+
+#define RRD_READONLY    (1<<0)
+#define RRD_READWRITE   (1<<1)
+#define RRD_CREAT       (1<<2)
+#define RRD_READAHEAD   (1<<3)
+#define RRD_COPY        (1<<4)
+#define RRD_EXCL        (1<<5)
 
 
 SEXP exposeArray(char c){
@@ -16,4 +32,64 @@ SEXP exposeArray(char c){
 
     return out;
 }
+
+SEXP importRRD(const char* filename){
+    rrd_t rrd;
+    unsigned int i, ii, ix, iii = 0;
+    rrd_file_t *rrd_file;
+    rrd_init(&rrd);
+    rrd_value_t my_cdp;
+    off_t     rra_base, rra_start, rra_next;
+
+    rrd_file = rrd_open(filename, &rrd, RRD_READONLY | RRD_READAHEAD);
+    if (rrd_file == NULL) {
+        rrd_free(&rrd);
+        return (-1);
+    }
+    rra_base = rrd_file->header_len;
+    rra_next = rra_base;
+
+    for (i = 0; i < rrd.stat_head->rra_cnt; i++) {
+
+        long      timer = 0;
+
+        rra_start = rra_next;
+        rra_next += (rrd.stat_head->ds_cnt
+                     * rrd.rra_def[i].row_cnt * sizeof(rrd_value_t));
+
+
+
+        rrd_seek(rrd_file, (rra_start + (rrd.rra_ptr[i].cur_row + 1)
+                            * rrd.stat_head->ds_cnt
+                            * sizeof(rrd_value_t)), SEEK_SET);
+
+        ii = rrd.rra_ptr[i].cur_row;
+
+	//for each archive
+        for (ix = 0; ix < rrd.rra_def[i].row_cnt; ix++) {
+            ii++;
+            if (ii >= rrd.rra_def[i].row_cnt) {
+                rrd_seek(rrd_file, rra_start, SEEK_SET);
+                ii = 0; /* wrap if max row cnt is reached */
+            }
+
+	    //for each data source 
+            for (iii = 0; iii < rrd.stat_head->ds_cnt; iii++) {
+                rrd_read(rrd_file, &my_cdp, sizeof(rrd_value_t) * 1);
+                if (isnan(my_cdp)) {
+		//TODO put symbol R_NaReal
+                } else {
+		//put number
+                }
+            }
+        }
+    }
+
+
+    rrd_free(&rrd);
+
+    rrd_close(rrd_file);
+
+}
+
 
