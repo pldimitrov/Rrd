4e7a4e6d263f13d5bca98729fa7dadd657a8de90
Plamen Dimitrov <dim.plamen@gmail.com>
2014-06-10 20:34:43 +0200

    now possible to import RRAs to a list of real vectors in R using rrd_fetch

--- R/Rrd.R
+++ R/Rrd.R
@@ -1,3 +1,11 @@
 importArray <- function() {
     .Call("exposeArray",'c', PACKAGE = "Rrd")
 }
+
+importRRD <- function(filename = "char", cf = "char", start = "integer", end = "integer", step = "long") {
+    .Call("importRRD", filename, cf, start, end, step, PACKAGE = "Rrd")
+}
+
+#importRRD <- function() {
+    #.Call("importRRD", 'c', PACKAGE = "Rrd")
+#}
--- src/Rrd.c
+++ src/Rrd.c
@@ -11,6 +11,7 @@
 /*#include "rrd_client.h"*/
 
 
+#define DEBUG
 
 #define RRD_READONLY    (1<<0)
 #define RRD_READWRITE   (1<<1)
@@ -33,63 +34,124 @@ SEXP exposeArray(char c){
     return out;
 }
 
-SEXP importRRD(const char* filename){
-    rrd_t rrd;
-    unsigned int i, ii, ix, iii = 0;
-    rrd_file_t *rrd_file;
-    rrd_init(&rrd);
-    rrd_value_t my_cdp;
-    off_t     rra_base, rra_start, rra_next;
+/*SEXP importRRD(const char* filename){*/
+    /*rrd_t rrd;*/
+    /*unsigned int i, ii, ix, iii = 0;*/
+    /*rrd_file_t *rrd_file;*/
+    /*rrd_init(&rrd);*/
+    /*rrd_value_t my_cdp;*/
+    /*off_t     rra_base, rra_start, rra_next;*/
 
-    rrd_file = rrd_open(filename, &rrd, RRD_READONLY | RRD_READAHEAD);
-    if (rrd_file == NULL) {
-        rrd_free(&rrd);
-        return (-1);
+    /*rrd_file = rrd_open(filename, &rrd, RRD_READONLY | RRD_READAHEAD);*/
+    /*if (rrd_file == NULL) {*/
+        /*rrd_free(&rrd);*/
+        /*return (-1);*/
+    /*}*/
+    /*rra_base = rrd_file->header_len;*/
+    /*rra_next = rra_base;*/
+
+    /*for (i = 0; i < rrd.stat_head->rra_cnt; i++) {*/
+
+        /*long      timer = 0;*/
+
+        /*rra_start = rra_next;*/
+        /*rra_next += (rrd.stat_head->ds_cnt*/
+                     /** rrd.rra_def[i].row_cnt * sizeof(rrd_value_t));*/
+
+
+
+        /*rrd_seek(rrd_file, (rra_start + (rrd.rra_ptr[i].cur_row + 1)*/
+                            /** rrd.stat_head->ds_cnt*/
+                            /** sizeof(rrd_value_t)), SEEK_SET);*/
+
+        /*ii = rrd.rra_ptr[i].cur_row;*/
+
+	/*//for each archive*/
+        /*for (ix = 0; ix < rrd.rra_def[i].row_cnt; ix++) {*/
+            /*ii++;*/
+            /*if (ii >= rrd.rra_def[i].row_cnt) {*/
+                /*rrd_seek(rrd_file, rra_start, SEEK_SET);*/
+                /*ii = 0; [> wrap if max row cnt is reached <]*/
+            /*}*/
+
+	    /*//for each data source */
+            /*for (iii = 0; iii < rrd.stat_head->ds_cnt; iii++) {*/
+                /*rrd_read(rrd_file, &my_cdp, sizeof(rrd_value_t) * 1);*/
+                /*if (isnan(my_cdp)) {*/
+		/*//TODO put symbol R_NaReal*/
+                /*} else {*/
+		/*//put number*/
+                /*}*/
+            /*}*/
+        /*}*/
+    /*}*/
+
+
+    /*rrd_free(&rrd);*/
+
+    /*rrd_close(rrd_file);*/
+
+/*}*/
+
+
+/*SEXP rrd_fetch(const char *filename, const char* cf,  time_t *start, time_t *end, unsigned long *step)  {*/
+/*SEXP importRRD(const char* filename, time_t start, time_t end, unsigned long step)  {*/
+SEXP importRRD(SEXP filenameIn, SEXP cfIn, SEXP startIn, SEXP endIn, SEXP stepIn)  {
+
+    rrd_value_t *data;
+    //this should complain if data needs to be initialized
+    //hmm... think we need to initialize
+    
+
+
+    /*const char filename[] = "/var/lib/ganglia/rrds/__SummaryInfo__/boottime.rrd";*/
+    const char *filename = CHAR(asChar(filenameIn));
+
+
+
+    /*const char cf[] = "AVERAGE";*/
+    const char *cf = CHAR(asChar(cfIn));
+
+    /*time_t start = 0;*/
+    time_t start = (time_t) asInteger(startIn);
+
+    /*time_t end = 0;*/
+    time_t end = (time_t) asInteger(endIn);
+
+
+    /*unsigned long step = 1;*/
+    unsigned long step = (unsigned long) asInteger(stepIn);
+
+    unsigned long ds_cnt;
+    char** ds_namv;
+
+    printf("we are about to call rrdfetch\n");
+    fflush(stdout);
+    int status = rrd_fetch_r(filename, cf, &start, &end, &step, &ds_cnt, &ds_namv, &data);
+    printf("we just did\n");
+    printf("size of data %d start %d end %d step %d ds_cnt %d\n", sizeof(data)/sizeof(rrd_value_t), start, end, step, ds_cnt);
+    fflush(stdout);
+    int size = end - start + 1;
+
+    SEXP out = PROTECT(allocVector(VECSXP, ds_cnt));
+    SEXP vec;
+    for (int ds = 0; ds < ds_cnt; ds++){
+	vec = PROTECT(allocVector(REALSXP, size));
+	for (int i = 0; i < size; i++){
+	    printf("iterating.. i = %d\n", i);
+	    /*REAL(out)[i] = *(data)[0][0];*/
+	    /*REAL(out)[i] = data[i];*/
+	    /*REAL(vec)[i] = (&data)[ds][i];*/
+	    REAL(vec)[i] = data[i + ds];
+	}
+	SET_VECTOR_ELT(out, ds, vec);
+	/*newdata += ds_cnt;*/
     }
-    rra_base = rrd_file->header_len;
-    rra_next = rra_base;
 
-    for (i = 0; i < rrd.stat_head->rra_cnt; i++) {
+    UNPROTECT(ds_cnt + 1);
 
-        long      timer = 0;
+    return out;
 
-        rra_start = rra_next;
-        rra_next += (rrd.stat_head->ds_cnt
-                     * rrd.rra_def[i].row_cnt * sizeof(rrd_value_t));
-
-
-
-        rrd_seek(rrd_file, (rra_start + (rrd.rra_ptr[i].cur_row + 1)
-                            * rrd.stat_head->ds_cnt
-                            * sizeof(rrd_value_t)), SEEK_SET);
-
-        ii = rrd.rra_ptr[i].cur_row;
-
-	//for each archive
-        for (ix = 0; ix < rrd.rra_def[i].row_cnt; ix++) {
-            ii++;
-            if (ii >= rrd.rra_def[i].row_cnt) {
-                rrd_seek(rrd_file, rra_start, SEEK_SET);
-                ii = 0; /* wrap if max row cnt is reached */
-            }
-
-	    //for each data source 
-            for (iii = 0; iii < rrd.stat_head->ds_cnt; iii++) {
-                rrd_read(rrd_file, &my_cdp, sizeof(rrd_value_t) * 1);
-                if (isnan(my_cdp)) {
-		//TODO put symbol R_NaReal
-                } else {
-		//put number
-                }
-            }
-        }
-    }
-
-
-    rrd_free(&rrd);
-
-    rrd_close(rrd_file);
 
 }
-
 
