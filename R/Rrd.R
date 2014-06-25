e8e2e2cba269ecee9c83d7bd9572f939b0b4beaf
Plamen Dimitrov <dim.plamen@gmail.com>
2014-06-25 19:20:01 +0200

    adding error checks

--- R/Rrd.R
+++ R/Rrd.R
@@ -5,15 +5,28 @@ importArray <- function() {
 importRRD <- function(filename = "char", cf = "char", start = "integer", end = "integer", step = "long") {
 
 
-    if (missing(filename))
+    if (missing(filename) || !is.character(filename))
 	stop("Need to provide a path to an RRD file")
 
 
+
     if (missing(cf) || missing(start) || missing(end) || missing(step)) {
+
 	.Call("smartImportRRD", filename, PACKAGE = "Rrd")
+
     } else {
+
+	if (!is.numeric(start) || !is.numeric(end) || !is.numeric(step)) 
+	    stop("start, end, step need to be numeric")
+
+	if (!is.character(cf))
+	    stop("consolidation function should be a string")
 
 	.Call("importRRD", filename, cf, start, end, step, PACKAGE = "Rrd")
     }
 
+}
+
+
+updateRRD <- function () {
 }
