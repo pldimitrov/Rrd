afd4090faee86ace931c96bb7616b56478d2844e
Plamen Dimitrov <dim.plamen@gmail.com>
2014-07-29 17:29:55 +0200

    implemented a getVal function that uses a per-package cache to store read-ahead RRD values

--- R/Rrd.R
+++ R/Rrd.R
@@ -1,3 +1,51 @@
+pkg.env <- new.env()
+pkg.env$rrdCache = list()
+
+setClass("RRD",
+	 representation(
+			filename="character",
+			cache="list"
+			)
+	 )
+
+RRD = function(filenameIn="character") {
+    if (missing(filenameIn) || !is.character(filenameIn))
+	stop("Need to provide a path to an RRD file")
+    if (!file.exists(filenameIn)) 
+	stop("File does not exist")
+    new ("RRD", filename=filenameIn, cache = list())
+}
+
+#setGeneric("getVal",
+	  getVal <- function(filename, cf, step, timestamp) {
+	      if (!(step %in% names(pkg.env$rrdCache[[filename]][[cf]]))) {
+		  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- importRRD(filename, cf, timestamp - 100, timestamp + 100, step)
+
+	      }
+
+	      else {
+		  if (is.na(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]][as.character(timestamp), ][1, 1])) {
+		      #TODO decide direction or read-ahead, store last timestamp asked
+		      tmpDF = importRRD(filename, cf, timestamp - 100, timestamp + 100, step)
+		      if (nrow(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]]) > 300) {
+			  print(nrow(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]]))
+			  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- tmpDF;
+		      }
+		      else {
+			  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- rbind(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]], tmpDF)
+
+		      }
+
+		  }
+	      }
+	      pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]][as.character(timestamp), ]
+
+	  }
+	 # )
+	  
+
+
+
 importArray <- function() {
     .Call("exposeArray",'c', PACKAGE = "Rrd")
 }
@@ -26,6 +74,9 @@ importRRD <- function(filename = "char", cf = "char", start = "integer", end = "
     }
 
 }
+
+
+
 
 
 updateRRD <- function () {
