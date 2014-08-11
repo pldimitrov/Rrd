26a8766de12eacc12cd05d47066baa9d4dc72bb0
Plamen Dimitrov <dim.plamen@gmail.com>
2014-08-11 05:29:20 +0200

    updated getVal to calcuate index from timestamp and  use an environment as a hash for the cache, updated documentation

--- R/Rrd.R
+++ R/Rrd.R
@@ -1,83 +1,74 @@
-pkg.env <- new.env()
-pkg.env$rrdCache = list()
+rrd.cache = new.env(hash = TRUE)
+rrd.cacheBlock = 50
 
-setClass("RRD",
-	 representation(
-			filename="character",
-			cache="list"
-			)
-	 )
 
-RRD = function(filenameIn="character") {
-    if (missing(filenameIn) || !is.character(filenameIn))
-	stop("Need to provide a path to an RRD file")
-    if (!file.exists(filenameIn)) 
-	stop("File does not exist")
-    new ("RRD", filename=filenameIn, cache = list())
+getVal <- function(filename, cf, step, timestamp) {
+	key  = paste(as.character(filename), as.character(cf), as.character(step), sep = "")
+	if (is.null(rrd.cache[[key]])) {
+
+		if (!file.exists(filename) || !is.character(cf) || !is.numeric(step) || !is.numeric(timestamp)) {
+			stop("usage: getVal(filename, cf, step, timestamp)")
+		}
+		rrd.cache[[key]] <- importRRD(filename, cf, timestamp - rrd.cacheBlock*step, timestamp + rrd.cacheBlock*step, step)
+		if (((timestamp - rrd.cache[[key]][1, 1]) %% step) != 0) {
+			stop("step is wrong")
+		}
+
+	}
+
+	else {
+		lastTimestamp = tail(rrd.cache[[key]], n = 1)[1, 1]
+
+		if (((timestamp - rrd.cache[[key]][1, 1]) %% step) != 0) {
+			stop("step is wrong")
+		}
+
+		if (timestamp < rrd.cache[[key]][1, 1]) {
+			tmpDF = importRRD(filename, cf, timestamp - rrd.cacheBlock*step, rrd.cache[[key]][1, 1] - step, step)
+			rrd.cache[[key]]  <- rbind(tmpDF, rrd.cache[[key]])
+
+		}
+
+
+		if (timestamp > lastTimestamp) {
+			tmpDF = importRRD(filename, cf, lastTimestamp + step, timestamp + rrd.cacheBlock*step, step)
+			rrd.cache[[key]] <- rbind(rrd.cache[[key]], tmpDF)
+		}
+
+	}
+
+	rrd.cache[[key]][(timestamp - rrd.cache[[key]][1, 1])/step + 1, ]
+
 }
 
-#setGeneric("getVal",
-	  getVal <- function(filename, cf, step, timestamp) {
-	      if (!(step %in% names(pkg.env$rrdCache[[filename]][[cf]]))) {
-		  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- importRRD(filename, cf, timestamp - 100, timestamp + 100, step)
-
-	      }
-
-	      else {
-		  if (is.na(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]][as.character(timestamp), ][1, 1])) {
-		      #TODO decide direction or read-ahead, store last timestamp asked
-		      tmpDF = importRRD(filename, cf, timestamp - 100, timestamp + 100, step)
-		      if (nrow(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]]) > 300) {
-			  print(nrow(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]]))
-			  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- tmpDF;
-		      }
-		      else {
-			  pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]] <- rbind(pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]], tmpDF)
-
-		      }
-
-		  }
-	      }
-	      pkg.env$rrdCache[[filename]][[cf]][[as.character(step)]][as.character(timestamp), ]
-
-	  }
-	 # )
-	  
 
 
-
-importArray <- function() {
-    .Call("exposeArray",'c', PACKAGE = "Rrd")
-}
 
 importRRD <- function(filename = "char", cf = "char", start = "integer", end = "integer", step = "long") {
 
 
-    if (missing(filename) || !is.character(filename))
-	stop("Need to provide a path to an RRD file")
+	if (missing(filename) || !is.character(filename))
+		stop("Need to provide a path to an RRD file")
 
 
 
-    if (missing(cf) || missing(start) || missing(end) || missing(step)) {
+	if (missing(cf) || missing(start) || missing(end) || missing(step)) {
 
-	.Call("smartImportRRD", filename, PACKAGE = "Rrd")
+		.Call("smartImportRRD", filename, PACKAGE = "Rrd")
 
-    } else {
+	} else {
 
-	if (!is.numeric(start) || !is.numeric(end) || !is.numeric(step)) 
-	    stop("start, end, step need to be numeric")
+		if (!is.numeric(start) || !is.numeric(end) || !is.numeric(step)) 
+			stop("start, end, step need to be numeric")
 
-	if (!is.character(cf))
-	    stop("consolidation function should be a string")
+		if (!is.character(cf))
+			stop("consolidation function should be a string")
 
-	.Call("importRRD", filename, cf, start, end, step, PACKAGE = "Rrd")
-    }
+		.Call("importRRD", filename, cf, start, end, step, PACKAGE = "Rrd")
+	}
 
 }
 
 
 
 
-
-updateRRD <- function () {
-}
--- README.md
+++ README.md
@@ -20,27 +20,59 @@ In R:
 
     library(Rrd)
 
+##importRRD(filename, consolidation function, start, end, step)
+Finds the RRA that best matches the __consolidation function__ and the __step__ and imports all values (from all data stores) in that RRA that are between timestamp __start__ and __end__.
+
+    rra = importRRD("filename.rrd", "AVERAGE", 1402144275, 1402144410, 15)
+
+Returns a data.frame object having the timestamp and the data stores as columns. The data store names are retrieved from the RRD file and set as the corresponding column names. The timestamps are also used as row names.
+
+    head(rra)
+
+                timestamp sum num
+    1402144275 1402144275   0   0
+    1402144290 1402144290   0   0
+    1402144305 1402144305   0   0
+    1402144320 1402144320   0   0
+    1402144335 1402144335   0   0
+    1402144350 1402144350   0   0
+
+
+
 ##importRRD(filename)
-Exposes all the values in an RRD - each RRA, each DS, from first to last timestamp.
+Retrieves the metadata from the RRD file and imports all values from this file - each RRA, each data store, from the first to the last timestamp. 
 
 
     rrd = importRRD("filename.rrd")
-    rrd[[i]][[j]][k]
 
-refers to the k<sup>th</sup> value (starting from the earliest timestamp for which the RRD stores data) from the j<sup>th</sup> data store in the i<sup>th</sup> RRA.
+Returns a list of data.frames (as above) with labels constructed as "consolidation function" + "step" - e.g. "AVERAGE15".
+
+    head(rrd$AVERAGE15)
+
+                timestamp sum num
+    1402144275 1402144275   0   0
+    1402144290 1402144290   0   0
+    1402144305 1402144305   0   0
+    1402144320 1402144320   0   0
+    1402144335 1402144335   0   0
+    1402144350 1402144350   0   0
 
 
-##importRRD(filename, consolidation function, start, end, step)
-Finds RRA that best matches the __consolidation function__ and the __step__ and imports all values (from all DS) in that RRA that are between timestamp __start__ and __end__.
 
-    rrd = importRRD("filename.rrd", "AVERAGE", 0, 0, 1)
-    rrd[[j]][k]
+##getVal(filename, consolidation function, step, timestamp)
 
-refers to the k<sup>th</sup> value (starting from timestamp __start__ until __end__) from the j<sup>th</sup> data store (DS) in the RRA that best matches the required __consolidation function__ and __step__.
+Returns a set of values (one for each data store) associated with a single timestamp. Reads small "chunks" of the RRA (see __rrd.cacheBlock__ parameter in source code) and uses a package-wide read-ahead cache implemented as an [environment](http://stat.ethz.ch/R-manual/R-devel/library/base/html/environment.html) object.
+
+    getVal("filename", "AVERAGE", 15, 1402231740)
+
+                timestamp sum num
+    1402231740 1402231740   0   0
+
+
+    rrd.cache$`filenameAVERAGE15`
+    
+
+ 
 
 
 For more information on rrdtool and the rrd format please refer to the official rrdtool [documentation](http://oss.oetiker.ch/rrdtool/doc/index.en.html) and [tutorials](http://oss.oetiker.ch/rrdtool/tut/index.en.html).
-    
-
-
-
