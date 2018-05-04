51199be0f17217475f976643fbe1c47299d63d75
andrie <andrie@rstudio.com>
2018-05-04 12:44:15 +0000

    Add examples

---
+++ inst/examples/example_read_rra.R
@@ -0,0 +1,11 @@
+rrd_cpu_0 <- system.file("extdata/cpu-0.rrd", package = "Rrd")
+
+start_time <- as.POSIXct("2018-05-01") # timestamp with data in example
+end_time <- as.POSIXct("2018-05-02") # timestamp with data in example
+avg_60 <- read_rra(rrd_cpu_0, cf = "AVERAGE", step = 60L, 
+                     start = start_time, 
+                     end = end_time)
+names(avg_60)
+head(avg_60)
+tail(avg_60)
+
---
+++ inst/examples/example_read_rrd.R
@@ -0,0 +1,10 @@
+rrd_cpu_0 <- system.file("extdata/cpu-0.rrd", package = "Rrd")
+
+cpu <- read_rrd(rrd_cpu_0)
+names(cpu)
+head(cpu[[1]])
+tail(cpu[[1]])
+
+tail(
+  cpu$AVERAGE60$sys
+)
